c7d86a38e419a7250085c5b594be4f0c83bfb3ad
John Wilson <johnwilson7mc@gmail.com>
2022-03-30 11:02:00 +0400

    header padding top script replace with static px

--- src/pages/home/home.js
+++
@@ -1,5 +0,0 @@
-export default (function Header(){
-    const header = document.querySelector('.hero')
-    const navbar = document.querySelector('.navbar')
-    header.style.paddingTop = navbar.clientHeight + 'px'
-})()
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,3 @@
 import swiper from './swiper.js'
 import observer from './observer.js'
 import {Button,Navbar} from '../components/index.js'
-import {Header} from '../pages/index.js'
